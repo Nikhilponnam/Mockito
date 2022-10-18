0622c991005395f3d17bc2031c5f7b9fd44d11ca
Nikhilponnam <ponnamnikhilkumar@gmail.com>
2022-10-18 22:55:21 +0530

    Remainaing files commit

---
+++ src/test/java/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
